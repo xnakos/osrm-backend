10746bf54580b4a4a56c01b0b2443ddbec7a7439
Harry Nakos <xnakos@gmail.com>
2015-12-08 14:32:57 +0200

    https://www.youtube.com/watch?v=HbwaTo3XhAs

--- extractor/extractor.cpp
+++ extractor/extractor.cpp
@@ -604,7 +604,7 @@ void extractor::WriteEdgeBasedGraph(std::string const &output_file_filename,
 
     for (const auto& edge : edge_based_edge_list) {
         file_out_stream.write((char *) &edge, sizeof(EdgeBasedEdge));
-        myEdgeBasedEdgesTxtFile << edge.source << " " << edge.target << " " << edge.edge_id << " " << edge.weight << " " << edge.forward << " " << edge.backward;
+        myEdgeBasedEdgesTxtFile << edge.source << " " << edge.target << " " << edge.edge_id << " " << edge.weight << " " << edge.forward << " " << edge.backward << std::endl;
     }
 
     TIMER_STOP(write_edges);
